8623cbbf14e291c77589977ac0adcbed154e5d86
souta-pqr <boriko0116@gmail.com>
2025-01-08 08:51:01 +0900

    add scripts

---
+++ data/scripts/text_utt2spk.py
@@ -0,0 +1,11 @@
+# ファイルを読み込む
+with open('text', 'r') as f:
+    lines = f.readlines()
+
+# 各行から半角空白より前の部分を抽出し，アンダーバー二つ目までの部分を追加
+extracted = [line.split(' ')[0] + " " + "_".join(line.split(' ')[0].split("_")[:2]) for line in lines]
+
+# 結果をファイルに書き込む
+with open('utt2spk', 'a') as f:
+    for item in extracted:
+        f.write("%s\n" % item)
